1a6da31fe150d9895064b19f75c9b56efc9feccc
PolinaPoliakova <poliakova_polya@mail.ru>
2025-05-31 10:46:52 +0300

    rollback improvement

--- common/src/main/java/org/project/group/Student.java
+++ common/src/main/java/org/project/group/Student.java
@@ -46,10 +46,6 @@ public class Student {
         return examResults;
     }
 
-    public double AverageGroupYear(){
-        return (double) (age + year) / 2;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) {
